26e499f8ca3dd8e756aa866966b26645019d59d8
Stephan Brandauer <stephan.brandauer@it.uu.se>
2014-11-03 13:08:26 +0100

    improved interactive.sh

--- interactive.sh
+++ interactive.sh
@@ -2,4 +2,15 @@
 
 t="1"
 make || t="0"
-while [ $t == "1" ] ; do clear && echo $t && ./main ; sleep 0.5; make || t="0"; done
+while [ true ]; do
+    while [ $t == "1" ] ; do
+	clear
+	./main
+	sleep 0.5
+	make || t="0"
+    done
+
+    while [ $t == "0" ]; do
+	(make &> /dev/null) && t="1"
+    done
+done
